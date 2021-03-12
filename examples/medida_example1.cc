3ca7f88a3348bcda450a91cc8f7c054b4f69a637
Hidenori Shinohara <hidenori@stellar.org>
2021-03-12 14:00:36 -0500

    update an example for my own reference

--- examples/medida_example1.cc
+++ examples/medida_example1.cc
@@ -1,9 +1,31 @@
 #include "medida/medida.h"
+#include <memory>
+#include <iostream>
+#include <sstream>
 
-int main(int argc, char* argv[]) {
-  medida::MetricsRegistry registry;
-  auto& counter = registry.NewCounter({"foo", "bar", "baz"});
-  counter.inc();
+namespace std {
+    template<typename T, typename... Args>
+        std::unique_ptr<T> make_unique(Args&&... args) {
+            return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+        }
+}
 
-  return 0;
+int main() {
+    std::unique_ptr<medida::MetricsRegistry> mMetrics = std::make_unique<medida::MetricsRegistry>();
+    medida::Timer& mLedgerClose = mMetrics->NewTimer({"ledger", "ledger", "close"});
+    auto ledgerTime = mLedgerClose.TimeScope();
+
+    ledgerTime.Stop();
+
+    medida::reporting::JsonReporter reporter(*mMetrics);
+
+    for (auto& kv : mMetrics->GetAllMetrics())
+    {
+        auto name = kv.first.ToString();
+        auto metric = kv.second;
+        metric->Process(reporter);
+    }
+
+    std::cout << reporter.Report() << std::endl;
+    return 0;
 }
