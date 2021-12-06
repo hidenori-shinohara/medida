ad047cfcfef56f53e20d115891dcfbec5045d60b
Hidenori Shinohara <hidenori@stellar.org>
2021-12-06 13:30:38 -0500

    copy and paste the ckms code from the prometheus-cpp repo

--- src/medida/stats/ckms.h
+++ src/medida/stats/ckms.h
@@ -2,18 +2,57 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
+#include <array>
+#include <cstddef>
+#include <functional>
+#include <vector>
+
 namespace medida {
 namespace stats {
 
 
 // TODO(hidenori): This file and the corresponding source file
 // is where the CKMS quantile estimation algorithm should be.
-class Ckms {
-    public:
-      // TODO(hidenori): Add more functions if necessary.
-      void add(double v);
-      double count() const;
-      double estimateQuantile(double q);
+
+class CKMSQuantiles {
+ public:
+  struct Quantile {
+    Quantile(double quantile, double error);
+
+    double quantile;
+    double error;
+    double u;
+    double v;
+  };
+
+ private:
+  struct Item {
+    double value;
+    int g;
+    int delta;
+
+    Item(double value, int lower_delta, int delta);
+  };
+
+ public:
+  explicit CKMSQuantiles(const std::vector<Quantile>& quantiles);
+
+  void insert(double value);
+  double get(double q);
+  void reset();
+
+ private:
+  double allowableError(int rank);
+  bool insertBatch();
+  void compress();
+
+ private:
+  const std::reference_wrapper<const std::vector<Quantile>> quantiles_;
+
+  std::size_t count_;
+  std::vector<Item> sample_;
+  std::array<double, 500> buffer_;
+  std::size_t buffer_count_;
 };
 
 } // namespace stats
