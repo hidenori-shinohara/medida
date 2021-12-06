779ad4a1e459e3113bce582742744a44d8593258
Hidenori Shinohara <hidenori@stellar.org>
2021-12-06 13:27:48 -0500

    add another test that doesn't pass yet
    (The tests are correct. They don't pass since there's no implementation
    and CKMS is just a placeholder now.)

--- test/stats/test_ckms.cc
+++ test/stats/test_ckms.cc
@@ -13,3 +13,12 @@ TEST(CkmsTest, aCkmsAddHundredOnes) {
   EXPECT_NEAR(1, ckms.estimateQuantile(1), 1e-6);
 }
 
+TEST(CkmsTest, aCkmsAddZeroToHundred) {
+  auto ckms = Ckms();
+  for (int i = 0; i <= 100; i++) {
+      ckms.add(i);
+  }
+  EXPECT_NEAR(50, ckms.estimateQuantile(0.5), 1e-6);
+  EXPECT_NEAR(99, ckms.estimateQuantile(0.99), 1e-6);
+  EXPECT_NEAR(100, ckms.estimateQuantile(1), 1e-6);
+}
